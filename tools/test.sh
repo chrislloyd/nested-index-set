351f3566da6b64f736a7a671fcbd09ba6c92740b
Chris Lloyd <chris@chrislloyd.net>
2023-01-04 22:44:54 +0000

    NPM package integration test
    I've had so many issues corectly setting up `package.json` files and publishing bad packages that this smoke test seems really valuable. It could be expanded to do actual assertions on logic later too.

--- tools/test.sh
+++ tools/test.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+# typescript check
 npx tsc --noEmit
+
+# npm package integration test
+# finds errors in the package.json configuration (main, type export etc.)
+{
+    package="$PWD/$(npm pack)"
+    tmpdir=$(mktemp -d)
+    pushd "$tmpdir"
+        npm init --yes
+        npm install ts-node "$package"
+        cat << EOF > index.ts
+import NestedIndexSet from '@chrislloyd/nested-index-set';
+console.log(new NestedIndexSet(0, 1, 2));
+EOF
+        output=$(npx ts-node index.ts)
+        expected="NestedIndexSet { min: 0, index: 1, max: 2 }"
+    popd 
+    rm -rf "$tmpdir" # cleanup
+} &> /dev/null
+test "$output" = "$expected" || echo "fail" "$output"
