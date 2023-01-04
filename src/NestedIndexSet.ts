d8f787926accca007a1f3e563531e9c615587fd1
Chris Lloyd <chris@chrislloyd.net>
2023-01-04 22:04:54 +0000

    TSDoc documentation

--- src/NestedIndexSet.ts
+++ src/NestedIndexSet.ts
@@ -1,7 +1,19 @@
 /**
- * TODO: TSDoc
+ * Captures relationships between fixed indexes without having to dynamically
+ * created. Indexes are guarenteed to be unique and can be created statically. 
+ * Typically used to model CSS `z-index` values.
+ * 
+ * @example
+ * ```ts
+ * const page = NestedIndex.fromRange(-999, 999);
+ * const modal = page.above();
+ * ```
  */
 export default class NestedIndexSet {
+    /**
+     * Returns a {@link NestedIndexSet} with its `index` at the midpoint of a 
+     * range.
+     */
     static fromRange(min: number, max: number) {
         const delta = max - min;
         const mid = Math.round(delta / 2);
@@ -9,6 +21,10 @@ export default class NestedIndexSet {
         return new NestedIndexSet(min, index, max);
     }
 
+    /**
+     * @throws RangeError
+     * Thrown if the range and index are not increasing, i.e. min < index < max
+     */
     constructor(
         public readonly min: number,
         public index: number,
@@ -19,10 +35,16 @@ export default class NestedIndexSet {
         }
     }
 
+    /**
+     * @returns A new set with a range greater than the current index
+     */
     above(): NestedIndexSet {
         return NestedIndexSet.fromRange(this.index + 1, this.max);
     }
 
+    /**
+     * @returns A new set with a range less than the current index
+     */
     below(): NestedIndexSet {
         return NestedIndexSet.fromRange(this.min, this.index - 1);
     }
